7ef232d11686003c3a5b1363cf930134dffa3a58
HBP87 <42292542+HBP87@users.noreply.github.com>
2020-01-15 19:07:00 +0530

    Update Solution.java

--- Java End-Of-File/Solution.java
+++ Java End-Of-File/Solution.java
@@ -1,22 +1,16 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-    Scanner userInput = new Scanner(System.in);
-    int lineNum = 1;    
-    
-    while (userInput.hasNextLine() == true) {
-    if (userInput.hasNext() == true) {
-        System.out.println(lineNum + " " + userInput.nextLine());
-        lineNum++;
-    }
-    }
-    
+        Scanner sc = new Scanner(System.in);
+        int lineNumber = 1;
+        while(sc.hasNextLine() == true)
+        {
+            System.out.println(n+" "+sc.nextLine());
+            lineNumber++;
+        }
     }
 }
+
